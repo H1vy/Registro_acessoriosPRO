531071ed241c49982dc7e82aa9f764831428c5bb
Hivy <hivysonmahve@gmail.com>
2026-03-17 21:03:10 -0300

    fix: force update v1.0.2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Registro_acessoriosPRO/',
-  // v1.0.1 - Trigger redeploy
+  // v1.0.2 - Force push to fix GH Pages blank screen
 })
